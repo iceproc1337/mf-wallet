dc5387103e2f1faa2c1e0b21a2b3d4df966a27dd
iceproc1337@proton.me <iceproc1337@proton.me>
2024-08-23 22:22:04 +0800

    add dev version of html and visualize plugin

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+import fs from 'fs/promises';
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
@@ -5,6 +6,9 @@ import vue from '@vitejs/plugin-vue'
 
 // tell vite to use externals
 import { viteExternalsPlugin } from 'vite-plugin-externals'
+
+// Visualize the bundle
+import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -16,6 +20,16 @@ export default defineConfig({
       "vue-i18n": "VueI18n",
       "pinia": "Pinia"
     }),
+    // Use index_dev.html in development
+    {
+      name: 'index-html-env',
+      async transformIndexHtml() {
+        if (process.env.NODE_ENV !== 'production') {
+          return await fs.readFile('index_dev.html', 'utf8')
+        }
+      }
+    },
+    visualizer({ open: true }),
   ],
   resolve: {
     alias: {
