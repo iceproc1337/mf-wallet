1f173d799c2f8cb9e5e49d245ada3ade71f0bf81
iceproc1337@proton.me <iceproc1337@proton.me>
2024-08-23 22:21:35 +0800

    extract dependencies

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,19 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+// tell vite to use externals
+import { viteExternalsPlugin } from 'vite-plugin-externals'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    viteExternalsPlugin({
+      "vue": "Vue",
+      "vue-router": "VueRouter",
+      "vue-i18n": "VueI18n",
+      "pinia": "Pinia"
+    }),
   ],
   resolve: {
     alias: {
